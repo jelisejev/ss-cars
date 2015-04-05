252b3574148127f0e462c605b66d0c8819524f20
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-04-06 01:59:18 +0300

    implemented a basic car list

--- ss-cars/routes/index.js
+++ ss-cars/routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
+var https = require('follow-redirects').https;
+var cheerio = require('cheerio');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // TODO: add second page
+  // TODO: move links to config
+  var url = 'https://www.ss.lv/ru/transport/cars/toyota/corolla/filter/fDgSeF4SEzwT.html';
+
+  https.get(url, function(response) {
+  console.log(response);
+    var body = '';
+
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    response.on('end', function() {
+      var list = parseResponse(body);
+
+      res.render('index', { ads: list });
+    });
+  });
 });
+
+function parseResponse(body) {
+  var $ = cheerio.load(body);
+
+  var list = [];
+  $('#filter_frm table').eq(2).find('tr').each(function() {
+    var row = $(this);
+
+    var price = row.find('.msga2-o').last().text();
+    // TODO: move price to config
+    if (!price || parseInt(price.replace(',', '')) > 6000) {
+      return;
+    }
+
+    list.push({
+      title: row.find('.msg2 a').text(),
+      price: row.find('.msga2-o').last().text()
+    });
+  });
+
+  return list;
+}
 
 module.exports = router;
