e102c3761aa378df1c94b3c3461c62676b7b54bd
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-04-06 11:33:11 +0300

    populated config file

--- ss-cars/config.js
+++ ss-cars/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     urls: [
+        'https://www.ss.lv/ru/transport/cars/toyota/corolla/filter/fDgSeF4SEzwT.html',
+        'https://www.ss.lv/ru/transport/cars/toyota/corolla/fDgTeF4SAzp4FD4eFFV8FQ==.html'
     ],
-    maxPrice: null
+    maxPrice: 6000
 };
