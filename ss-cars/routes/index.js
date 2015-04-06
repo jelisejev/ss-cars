a9d0191abaae569e7f88656e32478d3e8eb9e189
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-04-06 11:06:47 +0300

    implemented loading from multiple URLs

--- ss-cars/routes/index.js
+++ ss-cars/routes/index.js
@@ -1,27 +1,43 @@
 var express = require('express');
 var https = require('follow-redirects').https;
 var cheerio = require('cheerio');
+var async = require('async');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // TODO: add second page
   // TODO: move links to config
-  var url = 'https://www.ss.lv/ru/transport/cars/toyota/corolla/filter/fDgSeF4SEzwT.html';
+  var urls = [
+    'https://www.ss.lv/ru/transport/cars/toyota/corolla/filter/fDgSeF4SEzwT.html',
+    'https://www.ss.lv/ru/transport/cars/toyota/corolla/fDgTeF4SAzp4FD4eFFV8FQ==.html'
+  ];
 
-  https.get(url, function(response) {
-  console.log(response);
-    var body = '';
+  var functions = [];
+  urls.forEach(function(url) {
+    functions.push(function(callback) {
+      https.get(url, function(response) {
+        var body = '';
 
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
-      body += chunk;
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+          body += chunk;
+        });
+        response.on('end', function() {
+          var list = parseResponse(body);
+
+          callback(null, list);
+        });
+      });
     });
-    response.on('end', function() {
-      var list = parseResponse(body);
+  });
 
-      res.render('index', { ads: list });
+  async.parallel(functions, function(error, result) {
+    var ads = [];
+    result.forEach(function(list) {
+      ads = ads.concat(list);
     });
+
+    res.render('index', { ads: ads });
   });
 });
 
