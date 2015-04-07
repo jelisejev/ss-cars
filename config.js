22cf11aa51da21a08bc138b11d7ee23bfa12c492
pavel.yeliseev <pavel.yeliseev@ctco.lv>
2015-04-07 23:24:14 +0300

    removed config data

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    urls: [
-        'https://www.ss.lv/ru/transport/cars/toyota/corolla/filter/fDgSeF4SEzwT.html',
-        'https://www.ss.lv/ru/transport/cars/toyota/corolla/fDgTeF4SAzp4FD4eFFV8FQ==.html'
-    ],
-    maxPrice: 6000
+    urls: [],
+    maxPrice: null
 };
